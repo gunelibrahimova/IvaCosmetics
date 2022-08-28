10f1b92e1aa1083789a17e722fdc1e5ce28f7c12
gunelibrahimova <75317328+gunelibrahimova@users.noreply.github.com>
2022-08-29 00:55:31 +0400

    29.08.2022

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -67,6 +67,61 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
+                <div className="bottom">
+                    <div className="row justify-content-between">
+                        <div className="col-lg-3">
+                            <div className="image">
+                                <img width="80px" src="https://dtiva.wpengine.com/wp-content/uploads/2020/04/logo.png" alt="" />
+                            </div>
+                            <div className="icon">
+
+                                <p><i class="fa-solid fa-location-dot"></i>1203, Town Center Drive, FL 33458, USA</p>
+                                <p><i class="fa-solid fa-phone"></i>+841 123 456 78</p>
+                                <p><i class="fa-solid fa-envelope"></i>info@example.com</p>
+                            </div>
+                        </div>
+                        <div className="col-lg-3">
+                            <div className="text">
+                                <h3>Help</h3>
+                            </div>
+                            <div className="icon">
+                                <p>Search</p>
+                                <p>Help</p>
+                                <p>Information</p>
+                                <p>Privacy Policy</p>
+                                <p>Shipping Details</p>
+                            </div>
+                        </div>
+                        <div className="col-lg-3">
+                            <div className="text">
+                                <h3>Support</h3>
+                            </div>
+                            <div className="icon">
+                                <p>About us</p>
+                                <p>Careers</p>
+                                <p>Deliveries</p>
+                                <p>Refund Requests</p>
+                                <p>Contact us</p>
+                            </div>
+                        </div>
+                        <div className="col-lg-3">
+                            <div className="text">
+                                <h3>Information</h3>
+                            </div>
+                            <div className="icon">
+                                <p>Search Terms</p>
+                                <p>Advanced Search</p>
+                                <p>FAQ & Help Docs</p>
+                                <p>Orders & Returns</p>
+                                <p>Store Location</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="finish">
+                    <span>Copyright © 2020, Iva-theme | DesignThemes.</span>
+                </div>
             </div>
 
         </div>
